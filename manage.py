58743b13a49a7a0f85757842972be5851c1dcae3
viprs <viprs@qq.com>
2016-05-05 17:29:05 +0800

    添加了单元测试用例模块
    单元测试模块的脚手架已经完成

--- manage.py
+++ manage.py
@@ -16,5 +16,12 @@ def make_shell_context():
 manager.add_command('shell', Shell(make_context=make_shell_context()))
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    test_suite = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(test_suite)
+
 if __name__ == '__main__':
     manager.run()
