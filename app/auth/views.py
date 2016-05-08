0f3f2e99a555e0e058213e370f399622ce31464c
viprs <viprs@qq.com>
2016-05-08 11:27:18 +0800

    添加注册新用户业务逻辑
    1.注册新用户
    2.注册之前验证是否已经存在

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,10 +2,12 @@
 #encoding:utf-8
 __author__ = 'Samren'
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user, login_required, logout_user
+from flask.ext.login import login_user, login_required, logout_user, \
+current_user
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,6 +19,22 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password')
     return render_template('auth/login.html', form=form)
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('username: %s' % (form.username.data))
+        db.session.commit()
+        #token = user.generate_confirmation_token()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
 
 @auth.route('/logout')
 @login_required
