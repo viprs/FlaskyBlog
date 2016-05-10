fa951d270505156bf392791371d5582ad8173cff
viprs <viprs@qq.com>
2016-05-10 11:47:47 +0800

    添加服务器Markdown支持
    分三步：
    1.markdown函数把Markdown文本转换成HTML
    2.调用bleach.clean()删除不在白名单中的标签
    3.自动生成链接，调用likify()

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@
 __author__ = 'Samren'
 import datetime
 import hashlib
+from markdown import markdown
+import bleach
 from flask import current_app, request, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -31,6 +33,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -47,6 +50,17 @@ class Post(db.Model):
                      timestamp=forgery_py.date.date(True), author=u)
             db.session.add(p)
             db.session.commit()
+
+    @staticmethod
+    def on_change_body(target, value, oldvalue, initator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote',
+                        'code', 'em', 'i', 'li', 'ol', 'pre',
+                        'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+
+db.event.listen(Post.body, 'set', Post.on_change_body)
 
 
 class Role(db.Model):
