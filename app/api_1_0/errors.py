f588567d5a38398b2ef794f08038dd39c6a34750
viprs <viprs@qq.com>
2016-05-20 18:45:58 +0800

    添加评论模型

--- app/api_1_0/errors.py
+++ app/api_1_0/errors.py
@@ -5,6 +5,7 @@ from flask import jsonify
 from ..exceptions import ValidationError
 from . import api
 
+
 def bad_request(message):
     response = jsonify({'error': 'bad request', 'message': message})
     response.status_code = 400
--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ from flask import current_app, request, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask.ext.login import UserMixin, AnonymousUserMixin
+from exceptions import ValidationError
 from . import db
 from . import login_manager
 
@@ -78,6 +79,47 @@ class Post(db.Model):
         return Post(body=body)
 
 db.event.listen(Post.body, 'set', Post.on_change_body)
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True,
+                          default=datetime.datetime.utcnow)
+    disabled = db.Column(db.Boolean)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'code',
+                        'em', 'i', 'strong']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True
+        ))
+
+    def to_json(self):
+        json_comment = {
+            'url': url_for('api.get_comment', id=self.id, _external=True),
+            'post': url_for('api.get_post', id=self.post_id, _external=True),
+            'body': self.body,
+            'body_html': self.body,
+            'timestamp': self.timestamp,
+            'author': url_for('api.get_user', id=self.author_id, _external=True)
+        }
+        return json_comment
+
+    @staticmethod
+    def from_json(json_comment):
+        body = json_comment.get('body')
+        if body is None or body == '':
+            raise ValidationError('comment does not have a body')
+        return Comment(body=body)
+
+db.event.listen(Comment.body, 'set', Comment.on_changed_body)
 
 
 class Follow(db.Model):
