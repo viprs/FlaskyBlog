e0be3863f2d0dab201b9061e2f4c793866cc6669
viprs <viprs@qq.com>
2016-05-23 09:18:48 +0800

    添加获取所有用户的API

--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -6,6 +6,29 @@ from . import api
 from ..models import User, Post
 
 
+@api.route('/users/')
+def get_users():
+    page = request.args.get('page', 1, type=int)
+    pagination = User.query.paginate(page,
+        per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    users = pagination.items
+    prev_page = None
+    if pagination.has_prev:
+        prev_page = url_for('api.get_users', page=page-1, _external=True)
+    next_page = None
+    if pagination.has_next:
+        next_page = url_for('api.get_users', page=page+1, _external=True)
+    user_list = [user.to_json() for user in users]
+    return jsonify({
+        'users': user_list,
+        'prev_page': prev_page,
+        'next_page': next_page,
+        'total_count': pagination.total,
+        'page_count': user_list.__len__()
+    })
+
+
 @api.route('/users/<int:id>')
 def get_user(id):
     user = User.query.get_or_404(id)
